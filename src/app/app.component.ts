8e09b4a1c0cf0598ec5f99387abdad32fb3c2d56
lycHub <1120805364@qq.com>
2020-06-26 13:24:41 +0800

    over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+ /* template: `
+    <b>inline mode</b>
+    <h1 class="title">{{ title }}</h1>
+    <p>name: {{ heroName }}</p>
+  `,
+  styles: [`
+    b {
+        color: #6f42c1;
+    }
+  `]*/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hero';
+  heroName = '盖伦';
 }
