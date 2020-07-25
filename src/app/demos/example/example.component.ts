e8b73d8545b5d2a44eabae308b984857455b45b2
lycHub <1120805364@qq.com>
2020-07-25 11:12:06 +0800

    over

--- src/app/demos/example/example.component.ts
+++ src/app/demos/example/example.component.ts
@@ -3,7 +3,15 @@ import {
   OnInit,
   ViewEncapsulation
 } from '@angular/core';
-import {Observable} from 'rxjs';
+import {interval, Observable} from 'rxjs';
+
+const observable1 = interval(500);
+const observable2 = interval(300);
+
+const subscription = observable1.subscribe(x => console.log('first: ' + x));
+const childSubscription = observable2.subscribe(x => console.log('second: ' + x));
+
+subscription.add(childSubscription);
 
 @Component({
   selector: 'app-example',
@@ -20,36 +28,13 @@ export class ExampleComponent implements OnInit {
 
   }
   newObservable() {
-    const observable$ = new Observable(subscriber => {
-      // console.log('aaa');
-      subscriber.next(1);
-      subscriber.next(2);
-      subscriber.next(3);
-      // subscriber.error(new Error('出错了'));
-      subscriber.next(555);
-      subscriber.complete();
-      subscriber.next(444);
-    });
-    /*observable$.subscribe(res => {
-      console.log('res', res);
-    }, error => {
-      console.error('error', error);
-    }, () => {
-      console.log('complete');
-    });*/
-    observable$.subscribe({
-      next(res) {
-        console.log('res', res);
-      },
-      error(error) {
-        console.error('error', error);
-      },
-      complete() {
-        console.log('complete');
-      }
-    });
+    /*const observable$ = interval(1000);
+    const subscription = observable$.subscribe(x => console.log(x));
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 3000);*/
   }
   cancelObservable() {
-
+    subscription.unsubscribe();
   }
 }
