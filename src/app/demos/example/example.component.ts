514e7364b9ee73fb38bff42c3cdfad0679adede3
lycHub <1120805364@qq.com>
2020-08-01 08:09:06 +0800

    over

--- src/app/demos/example/example.component.ts
+++ src/app/demos/example/example.component.ts
@@ -3,19 +3,19 @@ import {
   OnInit,
   ViewEncapsulation
 } from '@angular/core';
-import {combineLatest, concat, forkJoin, fromEvent, interval, merge, of, partition, race, range, timer, zip} from 'rxjs';
+import {combineLatest, concat, forkJoin, fromEvent, interval, merge, of, partition, race, range, throwError, timer, zip} from 'rxjs';
 import {
   audit,
-  auditTime,
+  auditTime, catchError,
   combineAll,
-  concatAll, debounce, debounceTime, distinct, distinctUntilChanged, distinctUntilKeyChanged, elementAt,
+  concatAll, debounce, debounceTime, delay, distinct, distinctUntilChanged, distinctUntilKeyChanged, elementAt,
   endWith, filter, first, ignoreElements, last,
   map,
   mapTo,
-  mergeAll,
-  pluck, sample, sampleTime, single, skip, skipLast, skipUntil, skipWhile,
+  mergeAll, mergeMap,
+  pluck, retry, retryWhen, sample, sampleTime, single, skip, skipLast, skipUntil, skipWhile,
   startWith,
-  take, takeLast, takeUntil, takeWhile, throttle, throttleTime,
+  take, takeLast, takeUntil, takeWhile, tap, throttle, throttleTime,
   withLatestFrom
 } from 'rxjs/operators';
 
@@ -36,9 +36,28 @@ export class ExampleComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const interval$ = interval(500);
-    const result = interval$.pipe(throttleTime(2000));
-    result.subscribe(x => console.log(x));
+    const source = interval(1000);
+    const example = source.pipe(
+      map(val => {
+        if (val > 5) {
+          // 错误将由 retryWhen 接收
+          throw val;
+        }
+        return val;
+      }),
+      retryWhen(errors =>
+        errors.pipe(
+          // 输出错误信息
+          tap(val => console.log(`Value ${val} 太大了!`)),
+          // 3秒后重试
+          delay(3000)
+        )
+      )
+    );
+    const subscribe = example.subscribe(
+      val => console.log(val),
+      error => console.error('err ', error)
+    );
   }
   newObservable() {
 
