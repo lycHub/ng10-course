55d4f47f9d3607d50be8bf901ace18a3af42a67e
lycHub <1120805364@qq.com>
2020-09-05 15:43:40 +0800

    路由动画 over

--- src/app/pages/home/user-center/user-center.component.ts
+++ src/app/pages/home/user-center/user-center.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, HostBinding} from '@angular/core';
 import {UserService} from '../../../services/user.service';
 import {Hero} from '../../../configs/types';
 import {Observable} from 'rxjs';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-user-center',
   templateUrl: './user-center.component.html',
   styles: [
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('.user-center', [
+          style({opacity: 0, transform: 'translateY(-100px)'}),
+          stagger(300, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class UserCenterComponent implements OnInit {
   user$: Observable<Hero>;
@@ -16,5 +29,7 @@ export class UserCenterComponent implements OnInit {
     this.user$ = this.userServe.user$;
   }
 
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
   ngOnInit(): void {}
 }
