e7eaf2498366bba67b2a909f331c941f7c90803b
lycHub <1120805364@qq.com>
2020-09-10 08:26:50 +0800

    init

---
+++ src/app/core.module.ts
@@ -0,0 +1,20 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ]
+})
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule只能被AppModule引入');
+    }
+  }
+}
