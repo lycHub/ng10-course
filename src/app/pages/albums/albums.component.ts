2419d0258300e3168c3cb64aa98cf3b8b8aaa6be
lycHub <1120805364@qq.com>
2020-09-12 13:46:31 +0800

    over

--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
+import {AlbumArgs, AlbumService, CategoryInfo} from '../../services/apis/album.service';
 
 @Component({
   selector: 'xm-albums',
@@ -7,10 +8,29 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AlbumsComponent implements OnInit {
-
-  constructor() { }
+  searchParams: AlbumArgs = {
+    category: 'youshengshu',
+    subcategory: '',
+    meta: '',
+    sort: 0,
+    page: 1,
+    perPage: 30
+  };
+  categoryInfo: CategoryInfo;
+  constructor(
+    private albumServe: AlbumService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.updatePageData();
   }
 
+  private updatePageData(): void {
+    this.albumServe.detailCategoryPageInfo(this.searchParams).subscribe(categoryInfo => {
+      console.log('categoryInfo', categoryInfo);
+      this.categoryInfo = categoryInfo;
+      this.cdr.markForCheck();
+    });
+  }
 }
--- src/app/services/apis/album.service.ts
+++ src/app/services/apis/album.service.ts
@@ -3,7 +3,47 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {map} from 'rxjs/operators';
-import {Base, Category} from './types';
+import {Album, AlbumInfo, Anchor, Base, Category, MetaData, SubCategory, TracksInfo} from './types';
+import { stringify } from 'qs';
+
+export interface CategoryInfo {
+  category: Category;
+  currentSubcategory: SubCategory;
+  subcategories: SubCategory[];
+  metadata: MetaData[];
+}
+
+export interface AlbumsInfo {
+  albums: Album[];
+  page: number;
+  pageSize: number;
+  total: number;
+  pageConfig: { h1title: string };
+}
+
+export interface AlbumArgs {
+  category: string;
+  subcategory: string;
+  meta: string;
+  sort: number;
+  page: number;
+  perPage: number;
+}
+
+export interface AlbumRes {
+  albumId: number;
+  mainInfo: AlbumInfo;
+  anchorInfo: Anchor;
+  tracksInfo: TracksInfo;
+}
+
+export interface AlbumTrackArgs {
+  albumId: string;
+  sort: number;
+  pageNum: number;
+  pageSize: number;
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +58,13 @@ export class AlbumService {
     return this.http
       .get(`${environment.baseUrl}${this.prefix}breadcrumb`, { params })
       .pipe(map((res: Base<{ categories: Category[] }>) => res.data.categories));
+  }
+
+  // 二三级分类列表
+  detailCategoryPageInfo(args: Pick<AlbumArgs, 'category' | 'subcategory'>): Observable<CategoryInfo> {
+    const params = new HttpParams({ fromString: stringify(args) });
+    return this.http
+      .get(`${environment.baseUrl}${this.prefix}categories`, { params })
+      .pipe(map((res: Base<CategoryInfo>) => res.data));
   }
 }
