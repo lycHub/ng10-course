2557638e1f8993a2068b0632434cb8a5766d4e2e
lycHub <1120805364@qq.com>
2020-09-17 08:43:10 +0800

    over

--- src/app/share/components/pagination/pagination.component.ts
+++ src/app/share/components/pagination/pagination.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+type PageItemType = 'page' | 'prev' | 'next' | 'prev5' | 'next5';
+interface PageItem {
+  type: PageItemType;
+  num?: number;
+  disabled?: boolean;
+}
 
 @Component({
   selector: 'xm-pagination',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
-
+  @Input() total = 90;
+  @Input() pageNum = 1;
+  @Input() pageSize = 10;
+  private lastNum = 0;
+  listOfPageItems: PageItem[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.lastNum = Math.ceil(this.total / this.pageSize);
+    this.listOfPageItems = this.getListOfPageItems(this.pageNum, this.lastNum);
+    console.log('listOfPageItems', this.listOfPageItems);
   }
 
+  private getListOfPageItems(pageNum: number, lastNum: number): PageItem[] {
+    return concatWithPrevNext(generatePage(1, this.lastNum), pageNum, lastNum);
+  }
+}
+
+
+function generatePage(start: number, end: number): PageItem[] {
+  const list = [];
+  for (let i = start; i <= end; i++) {
+    list.push({
+      num: i,
+      type: 'page'
+    });
+  }
+  return list;
+}
+
+function concatWithPrevNext(listOfPage: PageItem[], pageNum: number, lastNum: number): PageItem[] {
+  return [
+    {
+      type: 'prev',
+      disabled: pageNum === 1
+    },
+    ...listOfPage,
+    {
+      type: 'next',
+      disabled: pageNum === lastNum
+    }
+  ];
 }
